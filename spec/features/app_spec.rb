67c675173ee5f3e0d8a22576bd223fed6ec5a08c
Seth Geyer <seth.geyer@gmail.com>
2014-07-22 12:00:46 -0600

    refactored app_spec tests

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -2,93 +2,63 @@ require "rspec"
 require "capybara"
 
 feature "Messages" do
+  before(:each) do
+    visit "/"
+  end
 
   scenario "As a user, I can submit a message" do
-    visit "/"
     expect(page).to have_content("Message Roullete")
-    fill_in "Message", :with => "Hello Everyone!"
-    click_button "Submit"
+    create_a_valid_message("Hello Everyone!")
     expect(page).to have_content("Hello Everyone!")
     expect(page).to have_link("Edit")
   end
 
   scenario "As a user, I see an error message if I enter a message > 140 characters" do
-    visit "/"
-    fill_in "Message", :with => "a" * 141
-    click_button "Submit"
+    create_an_invalid_message
     expect(page).to have_content("Message must be less than 140 characters.")
   end
 
   scenario "As a user, I can update a message" do
-    visit "/"
-    fill_in "Message", :with => "Hello Everyone!"
-    click_button "Submit"
+    create_a_valid_message("Hello Everyone!")
     click_on "Edit"
     expect(page).to have_content("Edit Message")
     expect(page.find("#message")).to have_content("Hello Everyone!")
-    fill_in "Message", with: "Hello World"
-    click_on "Submit"
+    create_a_valid_message("Hello World")
     expect(page).to have_content("Hello World")
     expect(page).to have_link("Edit")
   end
 
   scenario "As a user, I see an error message for messages > 140 chars" do
-    visit "/"
-    fill_in "Message", :with => "Hello Everyone!"
-    click_button "Submit"
+    create_a_valid_message("Hello Everyone!")
     click_on "Edit"
     expect(page).to have_content("Edit Message")
     expect(page.find("#message")).to have_content("Hello Everyone!")
-    fill_in "Message", with: "a" * 141
-    click_on "Submit"
+    create_an_invalid_message
     expect(page).to have_content("Message must be less than 140 characters.")
     expect(page).to have_content("Edit Message")
   end
 
 
   scenario "As a user, I can delete a message" do
-    visit "/"
-    fill_in "Message", :with => "Hello Everyone!"
-    click_button "Submit"
+    create_a_valid_message("Hello Everyone!")
     click_on "Delete"
     expect(page).not_to have_content("Hello Everyone")
   end
 
   scenario "As a user, I can add a comment" do
-    visit "/"
-    fill_in "Message", :with => "Hello Everyone!"
-    click_button "Submit"
-    click_on "Comment"
-    expect(page).to have_content("Add a Comment")
-    fill_in "Comment", with: "Good idea!"
-    click_on "Add Comment"
+    create_a_valid_message("Hello Everyone!")
+    create_a_comment("Good idea!")
     expect(page).to have_content("Good idea")
   end
 
   scenario "As a user, I can see/goto the individual message page" do
-    visit "/"
-    fill_in "Message", :with => "Hello Everyone!"
-    click_button "Submit"
-    click_on "Comment"
-    expect(page).to have_content("Add a Comment")
-    fill_in "Comment", with: "Good idea!"
-    click_on "Add Comment"
+    create_a_valid_message("Hello Everyone!")
+    create_a_comment("Good idea!")
     click_on "Hello Everyone!"
     expect(page).to have_content("Show Page")
     expect(page).to have_content("Good idea!")
     expect(page).not_to have_content("Edit Message")
   end
-
-  # As a user,
-  # I can click on a message and
-  # go to a page with only that message
-  # and its comments
-  #
-  # As a user
-  # When I visit the home page
-  # And I click on a message
-  # Then I should be on the message page
-  # And I should only see comments for that message
 
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,3 +19,20 @@ RSpec.configure do |config|
     database_connection.sql("ROLLBACK")
   end
 end
+
+
+def create_a_valid_message(message)
+  fill_in "Message", with: message
+  click_button "Submit"
+end
+
+def create_an_invalid_message
+  fill_in "Message", :with => "a" * 141
+  click_button "Submit"
+end
+
+def create_a_comment(comment)
+  click_on "Comment"
+  fill_in "Comment", with: comment
+  click_on "Add Comment"
+end
